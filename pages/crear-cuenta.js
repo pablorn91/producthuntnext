5cf58fe32b571faaf51ba6644b24409bdab584f0
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-26 13:33:58 -0400

    formulario crear cuenta nueva

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,12 +1,58 @@
 import React from 'react';
-
+import { css } from '@emotion/react'
 import Layout from "../components/layout/Layout"
+import { Formulario, Campo, InputSubmit } from '../components/ui/Formulario'
 
 export default function CrearCuenta() {
   return (
    <div>
        <Layout>
-        <h1>CrearCuenta</h1>
+        <>
+            <h1
+              css={css`
+                text-align: center;
+                margin-top: 5rem;
+              `}
+            >Crear Cuenta</h1>
+            <Formulario>
+              <Campo>
+                  <label htmlFor='nombre'>Nombre</label>
+                  <input
+                      type="text"
+                      id='nombre'
+                      placeholder='Tu Nombre'
+                      name='nombre'
+                  />
+              </Campo>
+
+              <Campo>
+                  <label htmlFor='email'>Email</label>
+                  <input
+                      type="email"
+                      id='email'
+                      placeholder='Tu Email'
+                      name='email'
+                  />
+              </Campo>
+
+              <Campo>
+                  <label htmlFor='password'>Password</label>
+                  <input
+                      type="password"
+                      id='password'
+                      placeholder='Tu Password'
+                      name='password'
+                  />
+              </Campo>
+
+              <InputSubmit
+                  type="submit"
+                  value="Crear Cuenta"
+              />
+
+            </Formulario>
+
+        </>
        </Layout>
    </div>
   )
