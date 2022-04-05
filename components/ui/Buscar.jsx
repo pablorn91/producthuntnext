2f654b6e28eef83600799052a62bb2bd65d83355
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-04-05 14:31:09 -0400

    busqueda

--- components/ui/Buscar.jsx
+++ components/ui/Buscar.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react'
+import Router from 'next/router';
 
 const InputText = styled.input`
   border: 1px solid var(--grisClaro2);
@@ -28,15 +29,33 @@ const InputSubmit = styled.button`
 `;
 
 const Buscar = () => {
+
+  const [ busqueda, setBusqueda ] = useState('');
+
+  const buscarProducto = e => {
+    e.preventDefault();
+    
+    if (busqueda.trim() === '') return;
+
+    //Redireccionar al usuario a /buscar
+    Router.push({
+      pathname: '/buscar',
+      query: { q : busqueda }
+    })
+
+  }
+
   return (
     <form
         css={css`
             position: relative;
         `}
+        onSubmit={buscarProducto}
     >
         <InputText 
             type="text"
-            placeholder="Buscar Productos" 
+            placeholder="Buscar Productos"
+            onChange={e => setBusqueda(e.target.value)}
         />
 
         <InputSubmit type="submit">Buscar</InputSubmit>
--- pages/buscar.js
+++ pages/buscar.js
@@ -1,12 +1,49 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 import Layout from "../components/layout/Layout"
+import { useRouter } from 'next/router';
+
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
 export default function Buscar() {
+  
+  const router = useRouter();
+  const { query: {q} } = router;
+
+  //Los productos
+  const { productos } = useProductos('creado')
+  const [ resultado, setResultado ] = useState([])
+
+  useEffect(() => {
+
+    const busqueda = q.toLowerCase();
+    const filtro = productos.filter( producto => {
+      return (
+        producto.nombre.toLowerCase().includes(busqueda) ||  producto.descripcion.toLowerCase().includes(busqueda)
+      )
+    })
+    setResultado(filtro)
+
+  }, [q, productos])
+
+  console.log(productos)
+
+  console.log(q)
   return (
    <div>
-       <Layout>
-        <h1>Buscar</h1>
+        <Layout>
+        <div className='listado-productos'>
+          <div className='contenedor'>
+            <ul className='bg-white'>
+              { resultado.map( producto => (
+                <DetallesProducto 
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
        </Layout>
    </div>
   )
